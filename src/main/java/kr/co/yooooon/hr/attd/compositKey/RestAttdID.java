a0ab00affdfa903996cca63694128136807fa767
Bottleon <qncn93@gmail.com>
2021-10-14 12:33:37 +0900

    수정 3

--- src/main/java/kr/co/yooooon/hr/attd/compositKey/RestAttdID.java
+++ src/main/java/kr/co/yooooon/hr/attd/compositKey/RestAttdID.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 
 @Data
 public class RestAttdID implements Serializable {
-    private Long restAttdCode;
+    private String restAttdCode;
     private String empCode;
 
 }
--- src/main/java/kr/co/yooooon/hr/certificate/to/ProofTO.java
+++ src/main/java/kr/co/yooooon/hr/certificate/to/ProofTO.java
@@ -1,6 +1,7 @@
 package kr.co.yooooon.hr.certificate.to;
 
 import kr.co.yooooon.base.to.BaseTO;
+import kr.co.yooooon.common.annotation.Dataset;
 import kr.co.yooooon.hr.certificate.compositKey.ProofCertificateCompositID;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,15 +13,16 @@ import javax.persistence.*;
 @Entity
 @Table(name="PROOF_LIST")
 @IdClass(ProofCertificateCompositID.class)
+@Dataset(name="ds_proof")
 public class ProofTO extends BaseTO {
 	//복합키 필요
 	@Id
-	private String seqNo;
+	private int seqNo;
 	@Id
 	private String empCode;
 
-	private String proofTypeCode,proofTypeName,startDate,position,dept,cash,cause,receipt,applovalStatus;
+	private String proofCode,proofName,requestDate,position,deptName,prootCost,reason,receipt,applovalstatus;
 
 	@Transient
-	private String empName;
+	private String empName,status;
 }
