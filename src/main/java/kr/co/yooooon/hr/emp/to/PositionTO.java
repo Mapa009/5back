848571d6690450ad7977cec3c831845b45869210
Bottleon <qncn93@gmail.com>
2021-10-18 16:39:54 +0900

    PositionTO

--- src/main/java/kr/co/yooooon/hr/emp/to/PositionTO.java
+++ src/main/java/kr/co/yooooon/hr/emp/to/PositionTO.java
@@ -10,35 +10,20 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
-<<<<<<< HEAD
-=======
-
-
->>>>>>> min
 
 @Data
 @Entity
 @Table(name="POSITION")
 @EqualsAndHashCode(callSuper=false)
-<<<<<<< HEAD
 @Dataset(name="ds_position")
-=======
-@Dataset(name="ds_baseSalary")
->>>>>>> min
 public class PositionTO extends BaseTO {
     @Id
     private String positionCode;
     
     @Column(nullable=false)
     private String position,baseSalary,hobongRatio,allowance;
-<<<<<<< HEAD
-
-    @Transient
-    private String status;
-=======
     
     //영속성에서 제거해주는 역할, 엑스플랫폼에서 dataset을 보면서 참고하자.
     @Transient 
     String status;
->>>>>>> min
 }
